2a81be93385e9621f79f0004ce9e52ea94dd56b6
David Murrell <david@murrell.co.nz>
2013-03-11 01:17:21 +1300

    Reworked gui a bit
    Suspect will need to make this dynamic. Underlying libray isn't good for
    pulling actual error data.
    Testing against test interface here:
    http://pjlink.jbmia.or.jp/english/STSD.html

--- ProjectorControl/ProjectorControl/Form1.Designer.cs
+++ ProjectorControl/ProjectorControl/Form1.Designer.cs
@@ -34,13 +34,17 @@
             this.ipAddress = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.Connect = new System.Windows.Forms.Button();
+            this.button_rgb = new System.Windows.Forms.Button();
+            this.button_video = new System.Windows.Forms.Button();
+            this.button_digital = new System.Windows.Forms.Button();
+            this.button_strage = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // On
             // 
-            this.On.Location = new System.Drawing.Point(12, 131);
+            this.On.Location = new System.Drawing.Point(300, 2);
             this.On.Name = "On";
-            this.On.Size = new System.Drawing.Size(200, 92);
+            this.On.Size = new System.Drawing.Size(134, 45);
             this.On.TabIndex = 0;
             this.On.Text = "On";
             this.On.UseVisualStyleBackColor = true;
@@ -48,9 +52,9 @@
             // 
             // Off
             // 
-            this.Off.Location = new System.Drawing.Point(238, 131);
+            this.Off.Location = new System.Drawing.Point(300, 56);
             this.Off.Name = "Off";
-            this.Off.Size = new System.Drawing.Size(200, 92);
+            this.Off.Size = new System.Drawing.Size(134, 48);
             this.Off.TabIndex = 1;
             this.Off.Text = "Off";
             this.Off.UseVisualStyleBackColor = true;
@@ -58,53 +62,98 @@
             // 
             // Status
             // 
-            this.Status.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Status.Location = new System.Drawing.Point(12, 9);
+            this.Status.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Status.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Status.Location = new System.Drawing.Point(12, 234);
             this.Status.Name = "Status";
-            this.Status.Size = new System.Drawing.Size(200, 68);
+            this.Status.Size = new System.Drawing.Size(422, 153);
             this.Status.TabIndex = 2;
             this.Status.Text = "Status";
-            this.Status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ipAddress
             // 
-            this.ipAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.ipAddress.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ipAddress.Location = new System.Drawing.Point(238, 94);
+            this.ipAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ipAddress.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipAddress.Location = new System.Drawing.Point(16, 75);
             this.ipAddress.Name = "ipAddress";
-            this.ipAddress.Size = new System.Drawing.Size(164, 30);
+            this.ipAddress.Size = new System.Drawing.Size(160, 29);
             this.ipAddress.TabIndex = 3;
-            this.ipAddress.Text = "192.168.1.1";
+            this.ipAddress.Text = "192.168.1.150";
             this.ipAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.ipAddress.WordWrap = false;
             this.ipAddress.TextChanged += new System.EventHandler(this.ipAddress_TextChanged);
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(98, 95);
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 22);
+            this.label1.Size = new System.Drawing.Size(168, 22);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Projector IP:";
+            this.label1.Text = "Projector Address";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             // 
             // Connect
             // 
-            this.Connect.Location = new System.Drawing.Point(238, 9);
+            this.Connect.Location = new System.Drawing.Point(16, 2);
             this.Connect.Name = "Connect";
-            this.Connect.Size = new System.Drawing.Size(200, 68);
+            this.Connect.Size = new System.Drawing.Size(164, 45);
             this.Connect.TabIndex = 5;
             this.Connect.Text = "Connect";
             this.Connect.UseVisualStyleBackColor = true;
             this.Connect.Click += new System.EventHandler(this.connect_Click);
             // 
+            // button_rgb
+            // 
+            this.button_rgb.Location = new System.Drawing.Point(16, 139);
+            this.button_rgb.Name = "button_rgb";
+            this.button_rgb.Size = new System.Drawing.Size(100, 48);
+            this.button_rgb.TabIndex = 6;
+            this.button_rgb.Text = "RGB";
+            this.button_rgb.UseVisualStyleBackColor = true;
+            this.button_rgb.Click += new System.EventHandler(this.button_vga_Click);
+            // 
+            // button_video
+            // 
+            this.button_video.Location = new System.Drawing.Point(122, 139);
+            this.button_video.Name = "button_video";
+            this.button_video.Size = new System.Drawing.Size(100, 48);
+            this.button_video.TabIndex = 7;
+            this.button_video.Text = "VIDEO";
+            this.button_video.UseVisualStyleBackColor = true;
+            this.button_video.Click += new System.EventHandler(this.button_video_Click);
+            // 
+            // button_digital
+            // 
+            this.button_digital.Location = new System.Drawing.Point(228, 139);
+            this.button_digital.Name = "button_digital";
+            this.button_digital.Size = new System.Drawing.Size(100, 48);
+            this.button_digital.TabIndex = 8;
+            this.button_digital.Text = "DIGITAL";
+            this.button_digital.UseVisualStyleBackColor = true;
+            this.button_digital.Click += new System.EventHandler(this.button_digital_Click);
+            // 
+            // button_strage
+            // 
+            this.button_strage.Location = new System.Drawing.Point(334, 139);
+            this.button_strage.Name = "button_strage";
+            this.button_strage.Size = new System.Drawing.Size(100, 48);
+            this.button_strage.TabIndex = 9;
+            this.button_strage.Text = "NETWORK";
+            this.button_strage.UseVisualStyleBackColor = true;
+            this.button_strage.Click += new System.EventHandler(this.button_network_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 239);
+            this.ClientSize = new System.Drawing.Size(446, 396);
+            this.Controls.Add(this.button_strage);
+            this.Controls.Add(this.button_digital);
+            this.Controls.Add(this.button_video);
+            this.Controls.Add(this.button_rgb);
             this.Controls.Add(this.Connect);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ipAddress);
@@ -114,7 +163,6 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,6 +176,10 @@
         private System.Windows.Forms.TextBox ipAddress;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button button_rgb;
+        private System.Windows.Forms.Button button_video;
+        private System.Windows.Forms.Button button_digital;
+        private System.Windows.Forms.Button button_strage;
     }
 }
 
--- ProjectorControl/ProjectorControl/Form1.cs
+++ ProjectorControl/ProjectorControl/Form1.cs
@@ -38,6 +38,8 @@ namespace ProjectorControl
             //turn on, update status
             Status.Text = "Turning Projector On";
             c.turnOn();
+            Status.Text = "Projector is now:" + c.powerQuery().ToString();
+            
         }
 
         private void Off_Click(object sender, EventArgs e)
@@ -45,6 +47,7 @@ namespace ProjectorControl
             //turn off, update status
             Status.Text = "Turning Projector Off";
             c.turnOff();
+            Status.Text = "Projector is now:" + c.powerQuery().ToString();
         }
 
 
@@ -63,8 +66,26 @@ namespace ProjectorControl
             if (System.Net.IPAddress.TryParse(ipAddress.Text, out address))
             {
                 Status.Text = "Connecting to " + address.ToString();
-                c = new PJLinkConnection(ipAddress.Text, "rv");
-                c.powerQuery();
+                c = new PJLinkConnection(ipAddress.Text, "JBMIAProjectorLink");
+                
+                LampStatusCommand l = new LampStatusCommand();
+                int hours = l.getHoursOfLamp(1);
+                string status = l.getStatusOfLamp(1).ToString();
+                string power = c.powerQuery().ToString();
+                ProjectorInfo pi = new ProjectorInfo();
+                Status.Text = "Connected. \n Projector is now: " + power + "\n" + "\nstatus: " + status + "\nlamphours: " + hours;
+                Status.Text += "\nFan:" + pi.FanStatus;
+                Status.Text += " Lamp:" + pi.LampStatus;
+                Status.Text += " Input:" + pi.Input;
+                Status.Text += "\nCover:" + pi.CoverStatus;
+                Status.Text += " Filter:" + pi.FilterStatus;
+                Status.Text += " NumLamps:" + pi.NumOfLamps;
+                Status.Text += "\nOthers:" + pi.PowerStatus;
+
+
+                
+                
+
             }
             else
             {
@@ -74,12 +95,44 @@ namespace ProjectorControl
 
             
         }
-
+        
 
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+            //c.sendCommand(
+        }
 
+
+        private void change_Input(rv.InputCommand.InputType x, int i)
+        {
+            InputCommand ic2 = new InputCommand(x, i);
+
+            if (c.sendCommand(ic2) == Command.Response.SUCCESS)
+                Console.WriteLine(ic2.dumpToString());
+            else
+                Console.WriteLine("Communication Error");
+
+        }
+
+        private void button_video_Click(object sender, EventArgs e)
+        {
+            change_Input(InputCommand.InputType.VIDEO,1);
+        }
+
+        private void button_vga_Click(object sender, EventArgs e)
+        {
+            change_Input(InputCommand.InputType.RGB,1);
+        }
+
+        private void button_digital_Click(object sender, EventArgs e)
+        {
+            change_Input(InputCommand.InputType.DIGITAL, 1);
+        }
+
+        private void button_network_Click(object sender, EventArgs e)
+        {
+            change_Input(InputCommand.InputType.NETWORK, 1);
         }
 
     }
